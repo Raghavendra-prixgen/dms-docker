9a3e320062efa2f4557ed36ade898c0ce4ddd3e9
raghavendra-prixgen <raghavendra.krishnamurthy@prixgen.com>
2026-07-14 17:43:56 +0530

    p

--- custom-addons/sale_discount_structure/__manifest__.py
+++ custom-addons/sale_discount_structure/__manifest__.py
@@ -3,7 +3,7 @@
     'description':'sale data structure',
     'summary' : "",
     
-    'version': '18.0.0.2',
+    'version': '18.0.0.1',
     'module_type':'official',
     'origin': 'base',
     
