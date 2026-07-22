e1f9c8e1522b3b88818681cdb776c626ba1e3d2e
Raghavendra-prixgen <raghavendra.krishnamurthy@prixgen.com>
2026-07-22 16:05:23 +0530

    [db:test]

--- custom-addons/vahini_dashboard/__manifest__.py
+++ custom-addons/vahini_dashboard/__manifest__.py
@@ -6,7 +6,7 @@
     'company': 'Prixgen Tech Solutions Pvt. Ltd.',
     'category': 'Uncategorized',
     'App origin': 'base',
-    'version': '18.0.0.222',
+    'version': '18.0.0.10101',
     'license': 'LGPL-3',
     'depends': ['web', 'account', 'sale'],
     'data': [
